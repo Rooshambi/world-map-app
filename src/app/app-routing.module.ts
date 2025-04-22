31a4085560d8a32bc2bcc7996d11f274dcf06b1f
jpacque <jennyedmond8@gmail.com>
2025-04-21 22:03:26 -0400

    D: configure Angular routing default to /map

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WorldComponent }       from './world/world.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',    redirectTo: 'map', pathMatch: 'full' },
+  { path: 'map', component: WorldComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule }    from '@angular/platform-browser';
+import { NgModule }         from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent }     from './app.component';
+import { WorldComponent }   from './world/world.component';
+import { AppRoutingModule } from './app‑routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorldComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
