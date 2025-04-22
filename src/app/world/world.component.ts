9b16f4821b8c1ed81a30b7db2a68f5680299aa06
jpacque <jennyedmond8@gmail.com>
2025-04-21 21:58:18 -0400

    chore: generate WorldComponent scaffold

---
+++ src/app/world/world.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-world',
+  standalone: false,
+  templateUrl: './world.component.html',
+  styleUrl: './world.component.css'
+})
+export class WorldComponent {
+
+}
